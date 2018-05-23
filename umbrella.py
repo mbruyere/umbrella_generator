1c6a416ec2de1c3dd47988e71a482a0fa3a79564
Marc Bruyere <mbruyere@nc.u-tokyo.ac.jp>
2018-05-23 17:06:27 +0900

    add function pixie_shownet and CLI main function entry

--- umbrella.py
+++ umbrella.py
@@ -55,6 +55,31 @@ def Braket(i):
     return yaml.load(i)
 
 
+def pixie_shownet(list_load):
+    ''' Function generating the faucet config for a single OF switch with Vlan'''
+    data = {'vlans': {vlan_name: {'vid': 100, 'description': qs(vlan_name)}}, 
+              'dps': {'sw1': {'dp_id': HexInt(dp_id_sw1), 'hardware': qs(sw1_type), 'interfaces': {1: {'name': qs('link'), 'description': qs('link'), 'acl_in': 1}}},
+                      'sw2': {'dp_id': HexInt(dp_id_sw2), 'hardware': qs(sw2_type), 'interfaces': {1: {'name': qs('link'), 'description': qs('link'), 'acl_in': 2}}},
+                      'sw2': {'dp_id': HexInt(dp_id_sw3), 'hardware': qs(sw3_type), 'interfaces': {1: {'name': qs('link'), 'description': qs('link'), 'acl_in': 3}}}},
+             'acls': {1: [{'rule': {'dl_dst': '00:00:00:00:00:01', 'actions': {'output': {'port': 1}}}}]}}
+
+    for i in range(len(list_load)):
+
+        data['dps']['sw1']['interfaces'][int(list_load[i]['port'])] = {'name': qs(list_load[i]['hostname']), 'description': qs(list_load[i]['hostname']), 'native_vlan': 100 ,'acl_in': 1}
+        
+        data['acls'][1].append({'rule': {'dl_vlan':int(list_load[i]['vlan']),'dl_dst': qs(list_load[i]['macaddr']), 'actions': {'output': {'port': int(list_load[i]['port'])}}}})
+        if IPv6_active == True:
+            data['acls'][1].append({'rule': {'dl_vlan':int(list_load[i]['vlan']),'dl_type': HexInt(0x86dd), 'ip_proto': 58, 'icmpv6_type': 135, 'ipv6_nd_target': qs(
+                list_load[i]['addr_ipv6']), 'actions': {'output': {'port': int(list_load[i]['port'])}}}})
+        
+        data['acls'][1].append({'rule': {'dl_vlan':int(list_load[i]['vlan']),'dl_type': HexInt(0x806), 'dl_dst': qs('ff:ff:ff:ff:ff:ff'), 'arp_tpa': qs(
+            list_load[i]['addr_ipv4']), 'actions': {'output': {'port': int(list_load[i]['port'])}}}})
+    
+    data['acls'][1].pop(0)
+    data['acls'][1].append({'rule': {'actions': {'allow': 0}}})
+    return(data)
+
+
 def one_switch(list_load):
     ''' Function generating the faucet config for a single OF switch'''
     data = {'vlans': {vlan_name: {'vid': 100, 'description': qs(vlan_name)}}, 'dps': {'sw1': {'dp_id': HexInt(dp_id_sw1), 'hardware': qs(sw1_type), 'interfaces': {1: {'name': qs(
@@ -327,6 +352,8 @@ if __name__ == '__main__':
             out_data = two_legacy(input_data)
         elif arguments['<topo_name>'] == 'triangle':
             out_data = triangle(input_data)
+        elif arguments['<topo_name>'] == 'pixie_shownet':
+            out_data = pixie_shownet(input_data)
 
         if len(out_data) != 0 :
             gen_faucet_yaml(out_data, arguments['<faucet_config_file_name>'])
